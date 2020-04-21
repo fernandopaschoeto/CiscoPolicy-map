56da0b0a8a8b87764510387ac464b051b83f0f99
LUIZ FERNANDO ARAUJO PASCHOETO <fernando.paschoeto@outlook.com>
2020-04-20 23:17:15 -0300

    Script completo
    Criado Script já com o comando e adicionado a opção de incluir a Vlan no script.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int MB, Kbytes, Bytes, vlan;
+        cout <<"\n\nInforme a Vlan do cliente: ";
+        cin >> vlan;
+        cout <<"\nInforme o valor em MB: ";
+        cin >>MB;
+        Bytes = 1048576 * MB;
+        Kbytes = 1024 * MB;
+        //cout << "\n\nPolicy-map receberá " << Bytes << " e " << Kbytes << ".\n\n";
+        cout << "#IMPUT \n\npolicy-map PM_VLAN" << vlan << "_INPUT \nclass CLASS_VLAN" << vlan << "_INPUT \npolice " << Bytes << " " << Kbytes << " conform-action set-dscp-transmit default exceed-action drop\n\n";
+        cout << "#OUTPUT \n\npolicy-map PM_VLAN" << vlan << "_OUTPUT \nclass CLASS_VLAN" << vlan << "_OUTPUT \npolice " << Bytes << " " << Kbytes << " conform-action set-dscp-transmit default exceed-action drop\n\n";
+}
